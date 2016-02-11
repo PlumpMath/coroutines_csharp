9891497b9b4295cd49cc2acf98061c23014b07d3
Marcel Valdez <marcel.valdez@live.com>
2016-02-10 21:09:30 -0500

    Make cosmetic improvements

--- Coroutines.Test/CoroutinesTest.cs
+++ Coroutines.Test/CoroutinesTest.cs
@@ -3,82 +3,77 @@ using System;
 using Coroutines;
 using System.Collections.Generic;
 
-namespace Coroutines.Test
-{
-	[TestFixture ()]
-	public class CoRoutinesTest
-	{
-		[Test ()]
-		public void TestCase_CanExecuteOne_OneAtATime ()
-		{
-			// Given
-			int count = 0;
-			Func<int> func =  () =>  ++count;
-			Func<int> func2 = () => ++count;
+namespace autCoroutines.Test {
+  [TestFixture()]
+  public class CoRoutinesTest {
+    [Test()]
+    public void TestCase_CanExecuteOne_OneAtATime() {
+      // Given
+      int count = 0;
+      Func<int> func = () => ++count;
+      Func<int> func2 = () => ++count;
 
-			IEnumerator<Job> jobs = func.AsCoRoutine ()
-				.ContinueWith (func2)
-				.ToJobs ();
+      IEnumerator<Job> jobs = func.AsCoRoutine()
+				.ContinueWith(func2)
+				.ToJobs();
 
-			// When
-			Job firstJob = jobs.DoWork ();
+      // When
+      Job firstJob = jobs.DoWork();
 	
-			// Then
-			Assert.AreEqual (1, count);
-			Assert.AreEqual (1, firstJob.Result);
-			Assert.AreEqual (Progress.InProgress, firstJob.Progress);
+      // Then
+      Assert.AreEqual(1, count);
+      Assert.AreEqual(1, firstJob.Result);
+      Assert.AreEqual(Progress.InProgress, firstJob.Progress);
 
-			// When
-			Job secondJob = jobs.DoWork();
+      // When
+      Job secondJob = jobs.DoWork();
 
-			// Then
-			Assert.AreEqual (2, count);
-			Assert.AreEqual (2, secondJob.Result);
-			Assert.AreEqual (Progress.InProgress, secondJob.Progress);
+      // Then
+      Assert.AreEqual(2, count);
+      Assert.AreEqual(2, secondJob.Result);
+      Assert.AreEqual(Progress.InProgress, secondJob.Progress);
 
-			// When
-			Job nullableJob = jobs.DoWork();
+      // When
+      Job nullableJob = jobs.DoWork();
 	
-			// Then
-			Assert.AreEqual (2, count); // There is no fnal job, the count should not increase
-			Assert.AreEqual (Job.NONE, nullableJob.Result);
-			Assert.AreEqual (Progress.Done, nullableJob.Progress);
-		}
+      // Then
+      Assert.AreEqual(2, count); // There is no fnal job, the count should not increase
+      Assert.AreEqual(Job.NONE, nullableJob.Result);
+      Assert.AreEqual(Progress.Done, nullableJob.Progress);
+    }
 
-		[Test ()]
-		public void TestCase_CanExecute_All ()
-		{
-			// Given
-			int count = 0;
-			Func<int> func =  () => ++count;
-			Func<int> func2 = () => ++count;
+    [Test()]
+    public void TestCase_CanExecute_All() {
+      // Given
+      int count = 0;
+      Func<int> func = () => ++count;
+      Func<int> func2 = () => ++count;
 
-			// When
-			func.AsCoRoutine ()
-				.ContinueWith (func2)
-				.ToJobs ()
-				.DoAllWork ();
+      // When
+      func.AsCoRoutine()
+				.ContinueWith(func2)
+				.ToJobs()
+				.DoAllWork();
 
-			// Then
-			Assert.AreEqual (2, count);
-		}
+      // Then
+      Assert.AreEqual(2, count);
+    }
 
-		[Test ()]
-		public void TestCase_OnlyExecutes_OnIteration ()
-		{
-			// Given
-			int count = 0;
-			Func<int> func =  () => ++count;
-			Func<int> func2 = () => ++count;
+    [Test()]
+    public void TestCase_OnlyExecutes_OnIteration() {
+      // Given
+      int count = 0;
+      Func<int> func = () => ++count;
+      Func<int> func2 = () => ++count;
 
-			// When
-			func.AsCoRoutine ()
-				.ContinueWith (func2)
-				.ToJobs ();
+      // When
+      func.AsCoRoutine()
+				.ContinueWith(func2)
+				.ToJobs();
 
-			// Then
-			Assert.AreEqual (0, count);
-		}
-	}
+      // Then
+      Assert.AreEqual(0, count);
+    }
+  }
 }
 
--- Coroutines/Coroutines.cs
+++ Coroutines/Coroutines.cs
@@ -1,141 +1,140 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Coroutines
-{
-	public static class Extensions {
+namespace Coroutines {
+  public static class Extensions {
 
-		/*
+    /*
 		 * Executes the next job in the pipeline, and returns it once it finishes.
 		 * */
-		public static Job DoWork(this IEnumerator<Job> jobs) {
-			jobs.MoveNext ();
-			return jobs.Current;
-		}
+    public static Job DoWork(this IEnumerator<Job> jobs) {
+      jobs.MoveNext();
+      return jobs.Current;
+    }
 
-		// Performs all of the work in the jobs
-		public static IList<object> DoAllWork(this IEnumerator<Job> jobs) {
-			IList<object> work = new List<object> ();
-			while(jobs.MoveNext()) {
-				work.Add (jobs.Current);
-			}
+    // Performs all of the work in the jobs
+    public static IList<object> DoAllWork(this IEnumerator<Job> jobs) {
+      IList<object> work = new List<object>();
+      while(jobs.MoveNext()) {
+        work.Add(jobs.Current);
+      }
 
-			return work;
-		}
+      return work;
+    }
 
-		public static IEnumerator<Job> ToJobs(this IEnumerable<CoRoutine> coroutines) {
-			return coroutines.GetJobs ().GetEnumerator ();
-		}
+    public static IEnumerator<Job> ToJobs(this IEnumerable<CoRoutine> coroutines) {
+      return coroutines.GetJobs().GetEnumerator();
+    }
 
-		// Executes a list of coroutines, and returns all of their jobs
-		// you must iterate over all the jobs in order for them to be
-		// executed
-		private static IEnumerable<Job> GetJobs(this IEnumerable<CoRoutine> coroutines) {
-			// A batch is executed as a single unit
-			// So a batch of batches would execute one complete batch at a time
-			CoRoutineBatch batch = new CoRoutineBatch (coroutines);
-			IEnumerator<Job> workInBatch = batch.execute ().GetEnumerator();
-			do {
-				// execute next coroutine
-				workInBatch.MoveNext ();
-				yield return workInBatch.Current;
-			} while (workInBatch.Current.Progress != Progress.Done);
-		}
+    // Executes a list of coroutines, and returns all of their jobs
+    // you must iterate over all the jobs in order for them to be
+    // executed
+    private static IEnumerable<Job> GetJobs(this IEnumerable<CoRoutine> coroutines) {
+      // A batch is executed as a single unit
+      // So a batch of batches would execute one complete batch at a time
+      CoRoutineBatch batch = new CoRoutineBatch(coroutines);
+      IEnumerator<Job> workInBatch = batch.execute().GetEnumerator();
+      do {
+        // execute next coroutine
+        workInBatch.MoveNext();
+        yield return workInBatch.Current;
+      } while(workInBatch.Current.Progress != Progress.Done);
+    }
 
-		// Groups together a list of coroutines, so they are executed as a single batch,
-		// this forces these coroutines to be executed as a single unit (all or none)
-		public static IEnumerable<CoRoutine> Batch<T>(this IEnumerable<CoRoutine> coroutines) {
-			yield return new CoRoutineBatch(coroutines);
-		}
+    // Groups together a list of coroutines, so they are executed as a single batch,
+    // this forces these coroutines to be executed as a single unit(all or none)
+    public static IEnumerable<CoRoutine> Batch<T>(this IEnumerable<CoRoutine> coroutines) {
+      yield return new CoRoutineBatch(coroutines);
+    }
 
-		public static IEnumerable<CoRoutine> ContinueWith<T>(this IEnumerable<CoRoutine> coroutines, Func<T> routine) {
-			foreach (CoRoutine coroutine in coroutines) {
-				yield return coroutine;
-			}
+    public static IEnumerable<CoRoutine> ContinueWith<T>(this IEnumerable<CoRoutine> coroutines, Func<T> routine) {
+      foreach(CoRoutine coroutine in coroutines) {
+        yield return coroutine;
+      }
 
-			foreach (CoRoutine coroutine in CoRoutine<T>.coroutine(routine)) {
-				yield return coroutine;
-			}
-		}
+      foreach(CoRoutine coroutine in CoRoutine<T>.coroutine(routine)) {
+        yield return coroutine;
+      }
+    }
 
-		public static IEnumerable<CoRoutine> AsCoRoutine<T>(this Func<T> routine) {
-			return CoRoutine<T>.coroutine (routine);
-		}
-	}
+    public static IEnumerable<CoRoutine> AsCoRoutine<T>(this Func<T> routine) {
+      return CoRoutine<T>.coroutine(routine);
+    }
+  }
 
-	public enum Progress {
-		Failed, Done, InProgress
-	}
+  public enum Progress {
+    Failed,
+    Done,
+    InProgress
+  }
 
-	public class Job {
-		public static object NONE = new object ();
+  public class Job {
+    public static object NONE = new object();
 
-		public Progress Progress {
-			get;
-			private set;
-		}
+    public Progress Progress {
+      get;
+      private set;
+    }
 
-		public object Result {
-			get;
-			private set;
-		}
+    public object Result {
+      get;
+      private set;
+    }
 
-		public static Job Done(object result) {
-			return new Job { Progress = Progress.Done, Result = result };
-		}
+    public static Job Done(object result) {
+      return new Job { Progress = Progress.Done, Result = result };
+    }
 
-		public static Job InProgress(object result) {
-			return new Job { Progress = Progress.InProgress, Result = result };
-		}
+    public static Job InProgress(object result) {
+      return new Job { Progress = Progress.InProgress, Result = result };
+    }
 
-		public static Job Failed(Exception exception) {
-			return new Job { Progress = Progress.Failed, Result = exception };
-		}
-	}
+    public static Job Failed(Exception exception) {
+      return new Job { Progress = Progress.Failed, Result = exception };
+    }
+  }
 
-	public interface CoRoutine {
-		IEnumerable<Job> execute ();
-	}
+  public interface CoRoutine {
+    IEnumerable<Job> execute();
+  }
 
-	class CoRoutine<T> : CoRoutine
-	{
-		private readonly Func<T> _routine;
+  class CoRoutine<T> : CoRoutine {
+    private readonly Func<T> _routine;
 
-		private CoRoutine (Func<T> routine)
-		{
-			this._routine = routine;
-		}
+    private CoRoutine(Func<T> routine) {
+      this._routine = routine;
+    }
 
-		public static IEnumerable<CoRoutine> coroutine<U>(Func<U> routine) {
-			yield return new CoRoutine<U> (routine);
-		}
+    public static IEnumerable<CoRoutine> coroutine<U>(Func<U> routine) {
+      yield return new CoRoutine<U>(routine);
+    }
 
-		public IEnumerable<Job> execute () {
-			yield return Job.Done (this._routine());
-		}
-	}
+    public IEnumerable<Job> execute() {
+      yield return Job.Done(this._routine());
+    }
+  }
 
-	class CoRoutineBatch : CoRoutine {
+  class CoRoutineBatch : CoRoutine {
 
-		private readonly IEnumerable<CoRoutine> routines;
+    private readonly IEnumerable<CoRoutine> routines;
 
-		public CoRoutineBatch(IEnumerable<CoRoutine> routines) {
-			this.routines = routines;
-		}
+    public CoRoutineBatch(IEnumerable<CoRoutine> routines) {
+      this.routines = routines;
+    }
 
-		#region CoRoutine implementation
-		public IEnumerable<Job> execute ()
-		{
-			foreach(CoRoutine routine in routines) {
-				foreach (Job job in routine.execute()) {
-					yield return Job.InProgress (job.Result);
-				}
-			}
+    #region CoRoutine implementation
 
-			yield return Job.Done (Job.NONE);
-		}
+    public IEnumerable<Job> execute() {
+      foreach(CoRoutine routine in routines) {
+        foreach(Job job in routine.execute()) {
+          yield return Job.InProgress(job.Result);
+        }
+      }
 
-		#endregion
-	}
+      yield return Job.Done(Job.NONE);
+    }
+
+    #endregion
+  }
 }
 
